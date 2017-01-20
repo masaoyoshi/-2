1d08039e9c50226d56d5ea0b8e9e85e0252051a7
ヨッシーまさお <ribery_muller_bayer@outlook.jp>
2017-01-20 14:36:04 +0900

    Add files via upload

---
+++ kadai2.m
@@ -0,0 +1,36 @@
+% ‰Û‘è‚Q@ŠK’²”‚Æ‹^Ž——ÖŠs
+% ‚QŠK’²C‚SŠK’²C‚WŠK’²‚Ì‰æ‘œ‚ð¶¬‚¹‚æD
+% ‰º‹L‚ÍƒTƒ“ƒvƒ‹ƒvƒƒOƒ‰ƒ€‚Å‚ ‚éD
+% ‰Û‘èì¬‚É‚ ‚½‚Á‚Ä‚ÍuLennavˆÈŠO‚Ì‰æ‘œ‚ð—p‚¢‚æD
+
+clear; % •Ï”‚ÌƒI[ƒ‹ƒNƒŠƒA
+
+ORG=imread('https://www.pakutaso.com/shared/img/thumb/MAX98_kazokugahueruyo20140531_TP_V.jpg'); % Œ´‰æ‘œ‚Ì“ü—Í
+ORG = rgb2gray(ORG); colormap(gray); colorbar;
+imagesc(ORG); axis image; % ‰æ‘œ‚Ì•\Ž¦
+pause; % ˆêŽž’âŽ~
+
+% ‚QŠK’²‰æ‘œ‚Ì¶¬
+IMG = ORG>128;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
+
+% ‚SŠK’²‰æ‘œ‚Ì¶¬
+IMG0 = ORG>64;
+IMG1 = ORG>128;
+IMG2 = ORG>192;
+IMG = IMG0 + IMG1 + IMG2;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
+
+% ‚WŠK’²‰æ‘œ‚Ì¶¬
+IMG3 = ORG>32
+IMG4 = ORG>64
+IMG5 = ORG>96
+IMG6 = ORG>128
+IMG7 = ORG>160
+IMG8 = ORG>192
+IMG9 = ORG>224
+IMG = IMG3 + IMG4 + IMG5 + IMG6 + IMG7 + IMG8 + IMG9
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
